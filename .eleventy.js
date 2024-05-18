3449a46660e0da4c871e98f2cff0f82c7e9a0b10
C.J. Wade <sudge64@icloud.com>
2024-05-17 22:08:47 -0500

    Change _site to docs

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,14 @@ module.exports = function(eleventyConfig) {
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  
+
   eleventyConfig.addCollection("posts", function(collection) {
       return collection.getFilteredByGlob("posts/**/*.md");
   });
+
+  return {
+    dir: {
+      output: "docs"
+    }
+  };
 }
