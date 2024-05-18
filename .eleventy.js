dd5fc17edc4f946bef37152d3a482f845246c335
C.J. Wade <sudge64@icloud.com>
2024-05-18 17:29:44 -0500

    Fix: Change python to Markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,14 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
 
-  eleventyConfig.addCollection("posts", function(collection) {
-      return collection.getFilteredByGlob("posts/**/*.md");
+  eleventyConfig.addCollection("posts", function (collection) {
+    return collection.getFilteredByGlob("posts/**/*.md");
   });
-
   return {
     dir: {
-      output: "docs"
-    }
+      output: "docs",
+    },
   };
-}
+};
